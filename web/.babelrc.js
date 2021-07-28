260088ce47f70702047b1802525cdbddf1c0a6c7
Ben Galloway <git@bengalloway.io>
2021-07-29 00:28:13 +0100

    Add and configure twin.macro

--- web/.babelrc.js
+++ web/.babelrc.js
@@ -1,2 +1,5 @@
 /** @type {import('@babel/core').TransformOptions} */
-module.exports = { extends: "../babel.config.js" }
+module.exports = {
+  extends: '../babel.config.js',
+  plugins: ['babel-plugin-macros', ['styled-components', { ssr: true }]],
+}
